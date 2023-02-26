cc31752104fff1ec4c2ed3021047116636161127
YoungSoulluoS <70120313+YoungSoulluoS@users.noreply.github.com>
2023-02-26 10:38:42 +0500

    Gas Gas Gaaaas to 0.7.3 Sodium 0.4.9

--- src/main/java/net/dorianpb/cem/internal/models/CemArmorModelPiglin.java
+++ src/main/java/net/dorianpb/cem/internal/models/CemArmorModelPiglin.java
@@ -2,20 +2,17 @@ package net.dorianpb.cem.internal.models;
 
 import net.dorianpb.cem.internal.api.CemModel;
 import net.dorianpb.cem.internal.config.CemConfigFairy;
-import net.dorianpb.cem.internal.api.CemEntityRenderFactory;
 import net.dorianpb.cem.internal.models.CemModelEntry.CemCuboid;
 import net.dorianpb.cem.internal.models.CemModelEntry.CemModelPart;
 import net.dorianpb.cem.internal.models.CemModelEntry.TransparentCemModelPart;
-import net.dorianpb.cem.external.CemEntitiesInit;
 import net.minecraft.client.model.ModelPart;
+import net.minecraft.client.model.ModelPart.Cuboid;
 import net.minecraft.client.render.entity.model.BipedEntityModel;
 import net.minecraft.client.render.entity.model.EntityModelPartNames;
 import net.minecraft.entity.LivingEntity;
-import net.minecraft.entity.mob.AbstractSkeletonEntity;
 import org.jetbrains.annotations.Nullable;
-import net.minecraft.entity.EntityType;
-import net.minecraft.entity.Entity;
 
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -27,25 +24,22 @@ public class CemArmorModelPiglin<C extends CemModel, T extends LivingEntity> ext
 	private static CemModelPart getRoot(CemModel model, @Nullable Float inflate){
 		CemModelPart root = new CemModelPart(64, 32);
 		if(CemConfigFairy.getConfig().useTransparentParts()){
-			root.addChild(EntityModelPartNames.HEAD, prepPart(castPart((CemModelPart) ((BipedEntityModel<?>) model).head), EntityModelPartNames.HEAD));
-			root.addChild(EntityModelPartNames.HAT, prepPart(castPart((CemModelPart) ((BipedEntityModel<?>) model).hat), EntityModelPartNames.HAT));
-			root.addChild(EntityModelPartNames.BODY, prepPart(castPart((CemModelPart) ((BipedEntityModel<?>) model).body), EntityModelPartNames.BODY));
-			root.addChild(EntityModelPartNames.RIGHT_ARM, prepPart(castPart((CemModelPart) ((BipedEntityModel<?>) model).rightArm), EntityModelPartNames.RIGHT_ARM));
-			root.addChild(EntityModelPartNames.LEFT_ARM, prepPart(castPart((CemModelPart) ((BipedEntityModel<?>) model).leftArm), EntityModelPartNames.LEFT_ARM));
-			root.addChild(EntityModelPartNames.RIGHT_LEG, prepPart(castPart((CemModelPart) ((BipedEntityModel<?>) model).rightLeg), EntityModelPartNames.RIGHT_LEG));
-			root.addChild(EntityModelPartNames.LEFT_LEG, prepPart(castPart((CemModelPart) ((BipedEntityModel<?>) model).leftLeg), EntityModelPartNames.LEFT_LEG));
+			root.addChild(EntityModelPartNames.HEAD, prepPart(castPart((CemModelPart) ((BipedEntityModel<?>) model).head), EntityModelPartNames.HEAD, inflate));
+			root.addChild(EntityModelPartNames.HAT, prepPart(castPart((CemModelPart) ((BipedEntityModel<?>) model).hat), EntityModelPartNames.HAT, inflate));
+			root.addChild(EntityModelPartNames.BODY, prepPart(castPart((CemModelPart) ((BipedEntityModel<?>) model).body), EntityModelPartNames.BODY, inflate));
+			root.addChild(EntityModelPartNames.RIGHT_ARM, prepPart(castPart((CemModelPart) ((BipedEntityModel<?>) model).rightArm), EntityModelPartNames.RIGHT_ARM, inflate));
+			root.addChild(EntityModelPartNames.LEFT_ARM, prepPart(castPart((CemModelPart) ((BipedEntityModel<?>) model).leftArm), EntityModelPartNames.LEFT_ARM, inflate));
+			root.addChild(EntityModelPartNames.RIGHT_LEG, prepPart(castPart((CemModelPart) ((BipedEntityModel<?>) model).rightLeg), EntityModelPartNames.RIGHT_LEG, inflate));
+			root.addChild(EntityModelPartNames.LEFT_LEG, prepPart(castPart((CemModelPart) ((BipedEntityModel<?>) model).leftLeg), EntityModelPartNames.LEFT_LEG, inflate));
 		}
 		else{
-			root.addChild(EntityModelPartNames.HEAD, prepPart(CemModelPart.of(((BipedEntityModel<?>) model).head), EntityModelPartNames.HEAD));
-			root.addChild(EntityModelPartNames.HAT, prepPart(CemModelPart.of(((BipedEntityModel<?>) model).hat), EntityModelPartNames.HAT));
-			root.addChild(EntityModelPartNames.BODY, prepPart(CemModelPart.of(((BipedEntityModel<?>) model).body), EntityModelPartNames.BODY));
-			root.addChild(EntityModelPartNames.RIGHT_ARM, prepPart(CemModelPart.of(((BipedEntityModel<?>) model).rightArm), EntityModelPartNames.RIGHT_ARM));
-			root.addChild(EntityModelPartNames.LEFT_ARM, prepPart(CemModelPart.of(((BipedEntityModel<?>) model).leftArm), EntityModelPartNames.LEFT_ARM));
-			root.addChild(EntityModelPartNames.RIGHT_LEG, prepPart(CemModelPart.of(((BipedEntityModel<?>) model).rightLeg), EntityModelPartNames.RIGHT_LEG));
-			root.addChild(EntityModelPartNames.LEFT_LEG, prepPart(CemModelPart.of(((BipedEntityModel<?>) model).leftLeg), EntityModelPartNames.LEFT_LEG));
-		}
-		if(inflate != null){
-			root.inflate(inflate);
+			root.addChild(EntityModelPartNames.HEAD, prepPart(CemModelPart.of(((BipedEntityModel<?>) model).head), EntityModelPartNames.HEAD, inflate));
+			root.addChild(EntityModelPartNames.HAT, prepPart(CemModelPart.of(((BipedEntityModel<?>) model).hat), EntityModelPartNames.HAT, inflate));
+			root.addChild(EntityModelPartNames.BODY, prepPart(CemModelPart.of(((BipedEntityModel<?>) model).body), EntityModelPartNames.BODY, inflate));
+			root.addChild(EntityModelPartNames.RIGHT_ARM, prepPart(CemModelPart.of(((BipedEntityModel<?>) model).rightArm), EntityModelPartNames.RIGHT_ARM, inflate));
+			root.addChild(EntityModelPartNames.LEFT_ARM, prepPart(CemModelPart.of(((BipedEntityModel<?>) model).leftArm), EntityModelPartNames.LEFT_ARM, inflate));
+			root.addChild(EntityModelPartNames.RIGHT_LEG, prepPart(CemModelPart.of(((BipedEntityModel<?>) model).rightLeg), EntityModelPartNames.RIGHT_LEG, inflate));
+			root.addChild(EntityModelPartNames.LEFT_LEG, prepPart(CemModelPart.of(((BipedEntityModel<?>) model).leftLeg), EntityModelPartNames.LEFT_LEG, inflate));
 		}
 		return root;
 	}
@@ -58,8 +52,8 @@ public class CemArmorModelPiglin<C extends CemModel, T extends LivingEntity> ext
 			return CemModelPart.of(modelPart);
 		}
 	}
-				
-	private static <M extends CemModelPart> M prepPart(M modelpart, String name){
+	
+	private static <M extends CemModelPart> M prepPart(M modelpart, String name, @Nullable Float inflate){
 		Map<CemModelPart, float[]> armorCandidates = new HashMap<>();
 		findArmorPlacement(modelpart, armorCandidates);
 		removeCuboids(modelpart, armorCandidates);
@@ -69,17 +63,21 @@ public class CemArmorModelPiglin<C extends CemModel, T extends LivingEntity> ext
 				float y = map[1];
 				float z = map[2];
 				boolean reverse = map[3] == 1;
+				float extra = inflate == null? 0 : inflate;
 				
-				var newarmor1 = new CemModelPart(64, 32);
+				Cuboid cuboid = switch(name){
+					case EntityModelPartNames.HEAD -> new CemCuboid(reverse? -8 : 1, 0, 0, 8, 8, 8, 1.5f*extra, 1.5f*extra, 1.5f*extra, false, false, 64, 32, 32, 0);
+					case EntityModelPartNames.HAT -> new CemCuboid(reverse? -8 : 1, 0, 0, 8, 8, 8, extra, extra, extra, false, false, 64, 32, 0, 0);
+					case EntityModelPartNames.BODY -> new CemCuboid(reverse? -8 : 1, 0, 0, 8, 12, 4, extra, extra, extra, false, false, 64, 32, 16, 16);
+					case EntityModelPartNames.RIGHT_ARM -> new CemCuboid(reverse? -4 : 0, 0, 0, 4, 12, 4, extra, extra, extra, false, false, 64, 32, 40, 16);
+					case EntityModelPartNames.LEFT_ARM -> new CemCuboid(reverse? -4 : 0, 0, 0, 4, 12, 4, extra, extra, extra, true, false, 64, 32, 40, 16);
+					case EntityModelPartNames.RIGHT_LEG -> new CemCuboid(reverse? -4 : 0, 0, 0, 4, 12, 4, extra, extra, extra, false, false, 64, 32, 0, 16);
+					case EntityModelPartNames.LEFT_LEG -> new CemCuboid(reverse? -4 : 0, 0, 0, 4, 12, 4, extra, extra, extra, true, false, 64, 32, 0, 16);
+					default -> null;
+				};
+				
+				var newarmor1 = new CemModelPart(64, 32, cuboid == null? Collections.emptyList() : Collections.singletonList(cuboid));
 				newarmor1.setPivot(x, y, z);
-				switch(name){
-					case EntityModelPartNames.HAT -> newarmor1.addCuboid(reverse? -8 : 1, 0, 0, 8, 8, 8, 0, false, false, 0, 0);
-					case EntityModelPartNames.BODY -> newarmor1.addCuboid(reverse? -8 : 0, 0, 0, 8, 12, 4, 0, false, false, 16, 16);
-					case EntityModelPartNames.RIGHT_ARM -> newarmor1.addCuboid(reverse? -4 : 0, 0, 0, 4, 12, 4, 0, false, false, 40, 16);
-					case EntityModelPartNames.LEFT_ARM -> newarmor1.addCuboid(reverse? -4 : 0, 0, 0, 4, 12, 4, 0, true, false, 40, 16);
-					case EntityModelPartNames.RIGHT_LEG -> newarmor1.addCuboid(reverse? -4 : 0, 0, 0, 4, 12, 4, 0, false, false, 0, 16);
-					case EntityModelPartNames.LEFT_LEG -> newarmor1.addCuboid(reverse? -4 : 0, 0, 0, 4, 12, 4, 0, true, false, 0, 16);
-				}
 				armorPart.addChild("armor", newarmor1);
 			}
 		});
@@ -87,7 +85,7 @@ public class CemArmorModelPiglin<C extends CemModel, T extends LivingEntity> ext
 	}
 	
 	private static <M extends CemModelPart> void findArmorPlacement(M modelpart, Map<CemModelPart, float[]> armorCandidates){
-		if(modelpart.children.size() == 0 || modelpart.cuboids.size() > 0){
+		if(modelpart.children.isEmpty() || !modelpart.cuboids.isEmpty()){
 			armorCandidates.put(modelpart, null);
 		}
 		else{
@@ -102,7 +100,7 @@ public class CemArmorModelPiglin<C extends CemModel, T extends LivingEntity> ext
 	private static <M extends ModelPart> void removeCuboids(M modelPart, Map<CemModelPart, float[]> armorCandidates){
 		if(armorCandidates.containsKey((CemModelPart) modelPart)){
 			for(ModelPart.Cuboid cuboid : modelPart.cuboids){
-				armorCandidates.put((CemModelPart) modelPart, new float[]{cuboid.minX, cuboid.minY, cuboid.minZ, ((CemCuboid) cuboid).isMirrorU()? 0 : 0});
+				armorCandidates.put((CemModelPart) modelPart, new float[]{cuboid.minX, cuboid.minY, cuboid.minZ, ((CemCuboid) cuboid).isMirrorU()? 1 : 0});
 			}
 		}
 		modelPart.cuboids.clear();
